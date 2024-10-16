35444e930edb369cabc387974e09bc1a01901944
Denis Bulgakov <praeitor@gmail.com>
2024-10-16 20:45:29 +0400

    first part commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/praeitor/go-devops-engeneer-magistr-lesson1
+
+go 1.23.2
--- main.go
+++ main.go
@@ -1,3 +1,35 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func main() {
+	url := "http://srv.msk01.gigacorp.local/_stats"
+	client := &http.Client{}
+
+	for {
+		resp, err := client.Get(ulr)
+		if err != nil {
+			fmt.Println("Error fetching data:", err)
+			time.Sleep(10 * time.Second)
+			continue
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nill {
+			fmt.Println("Error reading response body:", err)
+			resp.Body.Close()
+			time.Sleep(10 * time.Second)
+			continue
+		}
+
+		fmt.Println("Data received:", string(body))
+		resp.Body.Close()
+
+		time.Sleep(10 * time.Second)
+	}
+}
