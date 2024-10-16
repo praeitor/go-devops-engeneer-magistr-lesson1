30edca4d94707a21ea799a3ec495d2030fdf2b47
Denis Bulgakov <praeitor@gmail.com>
2024-10-16 21:20:29 +0400

    delete data received add base data processing

--- main.go
+++ main.go
@@ -4,17 +4,34 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 	"time"
 )
 
 func main() {
 	url := "http://srv.msk01.gigacorp.local/_stats"
 	client := &http.Client{}
+	errorCount := 0
 
 	for {
 		resp, err := client.Get(url)
 		if err != nil {
 			fmt.Println("Error fetching data:", err)
+			errorCount++
+			if errorCount >= 3 {
+				fmt.Println(("Unaable to fetch server stat"))
+			}
+			time.Sleep(10 * time.Second)
+			continue
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			fmt.Println("Non-200 response received:", resp.Status)
+			errorCount++
+			if errorCount >= 3 {
+				fmt.Println("Unable to fetch server statistic")
+			}
+			resp.Body.Close()
 			time.Sleep(10 * time.Second)
 			continue
 		}
@@ -27,7 +44,18 @@ func main() {
 			continue
 		}
 
-		fmt.Println("Data received:", string(body))
+		data := strings.Split(string(body), ",")
+		if len(data) < 6 {
+			fmt.Println("Invalid format data")
+			errorCount++
+			if errorCount >= 3 {
+				fmt.Println("Unable to fetch server stat")
+			}
+			resp.Body.Close()
+			time.Sleep(10 * time.Second)
+			continue
+		}
+
 		resp.Body.Close()
 
 		time.Sleep(10 * time.Second)
